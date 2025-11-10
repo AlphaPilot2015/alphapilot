807c18cd89db0e70f36939ef64a2c0a32b4c8cd4
AlphaPilot2015 <rvegassaez@gmail.com>
2025-11-10 13:55:33 -0500

    Update start.sh

--- AlphaPilotHosted/start.sh
+++ AlphaPilotHosted/start.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 set -e
 : "${PORT:=8080}"
-
-# Rellena la plantilla de Nginx con $PORT
 envsubst < /etc/nginx/nginx.tmpl.conf > /etc/nginx/nginx.conf
-
-# Arranca supervisor (api + nginx)
 exec /usr/bin/supervisord -n
+
